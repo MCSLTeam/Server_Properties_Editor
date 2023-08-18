a828d971a956545200b285465a369b70ed553f15
LxHTT <lxhtz.dl@qq.com>
2023-08-18 12:42:02 +0800

    A| 载入点

--- Server_Properties_Editor.py
+++ Server_Properties_Editor.py
@@ -1,29 +1,43 @@
 from PyQt5.QtCore import Qt
 from Adapters.Plugin import Plugin
 from MCSL2Lib.windowInterface import Window
-from qfluentwidgets import InfoBar, InfoBarPosition
+from .speInterface import SPEMainUI
+from qfluentwidgets import InfoBar, InfoBarPosition, FluentIcon as FIF
 
 Server_Properties_Editor = Plugin()
-
+speMainUI = SPEMainUI()
 
 def load():
     pass
 
 
 def enable():
-    InfoBar.success(
-        title="插件加载提示",
-        content="Server_Properties_Editor启用成功！",
-        orient=Qt.Horizontal,
-        isClosable=True,
-        position=InfoBarPosition.BOTTOM_LEFT,
-        duration=2500,
-        parent=Window().pluginsInterface,
-    )
+    try:
+        Window().addSubInterface(speMainUI, FIF.APPLICATION, "示例")
+        InfoBar.success(
+            title="插件加载提示",
+            content="Server_Properties_Editor启用成功！",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_LEFT,
+            duration=2500,
+            parent=Window().pluginsInterface,
+        )
+    except Exception as e:
+        InfoBar.error(
+            title="插件加载提示",
+            content=f"Server_Properties_Editor启用失败！\n{e}",
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_LEFT,
+            duration=2500,
+            parent=Window().pluginsInterface,
+        )
 
 
 def disable():
-    Window
+    # Window().
+    pass
 
 
 # 注册加载代码
